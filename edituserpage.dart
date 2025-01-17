b7c103f46260f4fa899c792289e06ccd27904d4c
Loay Sherif <143604561+Loaysherif@users.noreply.github.com>
2025-01-17 13:49:58 +0200

    Input Validation
    Add input validation for first name, last name, and email text fields to improve functionality and error handling for editUserPage

---
+++ edituserpage.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:user_profile_management/models/user.dart';
+
+class EditUserPage extends StatefulWidget {
+  final User user;
+  final Function() onUpdate;
+
+  const EditUserPage({super.key, required this.user, required this.onUpdate});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _EditUserPageState createState() => _EditUserPageState();
+}
+
+class _EditUserPageState extends State<EditUserPage> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController _firstNameController;
+  late TextEditingController _lastNameController;
+  late TextEditingController _emailController;
+
+  @override
+  void initState() {
+    super.initState();
+    _firstNameController = TextEditingController(text: widget.user.firstName);
+    _lastNameController = TextEditingController(text: widget.user.lastName);
+    _emailController = TextEditingController(text: widget.user.email);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit User'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormField(
+                controller: _firstNameController,
+                decoration: InputDecoration(labelText: 'First Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a first name';
+                  } else if (!RegExp(r'^[a-zA-Z]+$').hasMatch(value)) {
+                    return 'First name can only contain letters';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _lastNameController,
+                decoration: InputDecoration(labelText: 'Last Name'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a last name';
+                  } else if (!RegExp(r'^[a-zA-Z]+$').hasMatch(value)) {
+                    return 'Last name can only contain letters';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                controller: _emailController,
+                decoration: InputDecoration(labelText: 'Email'),
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter an email';
+                  } else if (!RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+                      .hasMatch(value)) {
+                    return 'Please enter a valid email';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  if (_formKey.currentState!.validate()) {
+                    try {
+                      await widget.onUpdate();
+                      // ignore: use_build_context_synchronously
+                      Navigator.pop(context);
+                    } catch (e) {
+                      // ignore: use_build_context_synchronously
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Failed to update user: $e')),
+                      );
+                    }
+                  }
+                },
+                child: Text('Save'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
